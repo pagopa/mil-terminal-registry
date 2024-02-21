cb82fc38e37c74226b2a9d31590c6bbba79f7945
Andrea Morabito <andrea.morabito@pagopa.it>
2024-02-21 11:58:59 +0100

    fix utf-32 pattern validation

--- src/main/java/it/pagopa/swclient/mil/controller/model/PagoPaConf.java
+++ src/main/java/it/pagopa/swclient/mil/controller/model/PagoPaConf.java
@@ -5,16 +5,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record PagoPaConf(@NotNull
-                           @Pattern(regexp = "^[\\u0001-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFFF]{1,35}$")
-                           // fixme ^[\u0001-\uD7FF\uE000-\uFFFD\u1000-\u10FF]{1,35}$ verificare che la regex sia corretta
-                           String pspId,
+                         @Pattern(regexp = "^[\\x{0001}-\\x{D7FF}\\x{E000}-\\x{FFFD}\\x{10000}-\\x{10FFFF}]{1,35}$")
+                         String pspId,
                          @NotNull
-                           @Pattern(regexp = "^[\\u0001-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFFF]{1,35}$")
-                           // fixme ^[\u0001-\uD7FF\uE000-\uFFFD\u1000-\u10FF]{1,35}$ verificare che la regex sia corretta
-                           String brokerId,
+                         @Pattern(regexp = "^[\\x{0001}-\\x{D7FF}\\x{E000}-\\x{FFFD}\\x{10000}-\\x{10FFFF}]{1,35}$")
+                         String brokerId,
                          @NotNull
-                           @Pattern(regexp = "^[\\u0001-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFFF]{1,35}$")
-                           // fixme ^[\u0001-\uD7FF\uE000-\uFFFD\u1000-\u10FF]{1,35}$ verificare che la regex sia corretta
-                           String channelId) {
+                         @Pattern(regexp = "^[\\x{0001}-\\x{D7FF}\\x{E000}-\\x{FFFD}\\x{10000}-\\x{10FFFF}]{1,35}$")
+                         String channelId) {
 
 }
