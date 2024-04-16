6c29b842b78eff9ff7a46406782379384d18481e
Luca Consalvi <luca.consalvi@pagopa.it>
2024-04-16 12:29:14 +0200

    [MCIL-29] try fix

--- src/main/java/it/pagopa/swclient/mil/dao/PageMetadata.java
+++ src/main/java/it/pagopa/swclient/mil/dao/PageMetadata.java
@@ -1,4 +1,7 @@
 package it.pagopa.swclient.mil.dao;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record PageMetadata(int size, long totalElements, int totalPages) {
 }
--- src/main/java/it/pagopa/swclient/mil/dao/TerminalPageResponse.java
+++ src/main/java/it/pagopa/swclient/mil/dao/TerminalPageResponse.java
@@ -2,8 +2,5 @@ package it.pagopa.swclient.mil.dao;
 
 import java.util.List;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection
 public record TerminalPageResponse(List<TerminalEntity> terminals, PageMetadata page) {
 }
