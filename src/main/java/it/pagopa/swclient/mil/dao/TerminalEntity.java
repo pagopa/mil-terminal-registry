c502b5383f1f94fa0f63489e5c90b1279ff32a51
Luca Consalvi <luca.consalvi@pagopa.it>
2024-04-16 15:22:02 +0200

    [MCIL-29] add bsonproperty to terminal entity

--- src/main/java/it/pagopa/swclient/mil/dao/TerminalEntity.java
+++ src/main/java/it/pagopa/swclient/mil/dao/TerminalEntity.java
@@ -1,5 +1,7 @@
 package it.pagopa.swclient.mil.dao;
 
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import lombok.*;
@@ -18,4 +20,14 @@ public class TerminalEntity extends PanacheMongoEntity{
     private String serviceProviderId;
 
     private Terminal terminal;
+
+    public TerminalEntity(@BsonProperty("terminalUuid") String terminalUuid, 
+        @BsonProperty("terminalHandler") String terminalHandler,
+        @BsonProperty("serviceProviderId") String serviceProviderId,
+        @BsonProperty("terminal") Terminal terminal ){
+            this.terminalUuid = terminalUuid;
+            this.terminalHandler = terminalHandler;
+            this.serviceProviderId = serviceProviderId;
+            this.terminal = terminal;
+        }
 }
