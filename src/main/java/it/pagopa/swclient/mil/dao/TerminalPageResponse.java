af421134695d3364307fbda85d30f266c837415d
Luca Consalvi <luca.consalvi@pagopa.it>
2024-04-17 12:52:39 +0200

    [MCIL-29] add register for reflaction import

--- src/main/java/it/pagopa/swclient/mil/dao/TerminalPageResponse.java
+++ src/main/java/it/pagopa/swclient/mil/dao/TerminalPageResponse.java
@@ -1,6 +1,8 @@
 package it.pagopa.swclient.mil.dao;
 
 import java.util.List;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 @RegisterForReflection
 public record TerminalPageResponse(List<TerminalEntity> terminals, PageMetadata page) {
 }
