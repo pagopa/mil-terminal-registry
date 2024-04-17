2aaa8cd200abccff31e3a9a13d14da73d009d3a3
Luca Consalvi <luca.consalvi@pagopa.it>
2024-04-17 14:46:44 +0200

    [MCIL-29] test bson property

--- src/main/java/it/pagopa/swclient/mil/dao/TerminalEntity.java
+++ src/main/java/it/pagopa/swclient/mil/dao/TerminalEntity.java
@@ -1,14 +1,18 @@
 package it.pagopa.swclient.mil.dao;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
+import org.bson.codecs.pojo.annotations.BsonProperty;
 
 @MongoEntity(database = "mil", collection = "terminalRegistry")
 public class TerminalEntity {
 
+    @BsonProperty("terminalUuid")
     public String terminalUuid;
 
+    @BsonProperty("terminalHandler")
     public String terminalHandler;
 
+    @BsonProperty("serviceProviderId")
     public String serviceProviderId;
 
     public Terminal terminal;
