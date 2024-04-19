3ae93f1f70d65d34715f98486456375cf8b25282
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2024-04-19 15:16:09 +0200

    Test without BsonProperty

--- src/main/java/it/pagopa/swclient/mil/dao/TerminalEntity.java
+++ src/main/java/it/pagopa/swclient/mil/dao/TerminalEntity.java
@@ -1,7 +1,6 @@
 package it.pagopa.swclient.mil.dao;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
-import org.bson.codecs.pojo.annotations.BsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,13 +15,10 @@ import org.bson.codecs.pojo.annotations.BsonId;
 public class TerminalEntity {
 
     @BsonId
-    @BsonProperty("terminalUuid")
     private String terminalUuid;
 
-    @BsonProperty("terminalHandler")
     private String terminalHandler;
 
-    @BsonProperty("serviceProviderId")
     private String serviceProviderId;
 
     private Terminal terminal;
